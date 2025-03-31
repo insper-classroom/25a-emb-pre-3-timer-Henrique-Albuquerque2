9aff916b6427d0b3a1c7ab51a0b2e9a974bd9a0c
Henrique-Albuquerque2 <henriquea1@al.insperedu.br>
2025-03-31 05:42:32 +0000

    teste

--- exe3/main.c
+++ exe3/main.c
@@ -7,15 +7,15 @@ const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
 volatile bool timer_fired = false;
-
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     timer_fired = true;
     return 0;
 }
 
 void btn_callback(uint gpio, uint32_t events) {
+    alarm_id_t alarm;
     if (events == 0x4) { 
-        alarm_id_t alarm = add_alarm_in_ms(300, alarm_callback, NULL, false));
+        alarm = add_alarm_in_ms(1000, alarm_callback, NULL, false);
     } else if (events == 0x8) { 
         if (!timer_fired) {
             cancel_alarm(alarm);
@@ -23,20 +23,22 @@ void btn_callback(uint gpio, uint32_t events) {
             flag_f_r = 1;
             timer_fired = false;
         }
+    }
 }
-
-
+    
+    
 int main() {
     stdio_init_all();
-
+    
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
     gpio_put(LED_PIN_R, 0); 
-
+    
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
-
+        
+    
     gpio_set_irq_enabled_with_callback(
         BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
@@ -47,5 +49,6 @@ int main() {
             sleep_ms(10); 
         }
 
-        return 0;
-}}
+    }
+    return 0;
+}
